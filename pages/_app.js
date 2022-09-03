b6beb3a16b4d8c07549b474f51a1e4cd5e648205
Maciejka1 <maciekglab0812@vp.pl>
2022-09-03 20:43:17 +0200

    fix fonts

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,9 @@ function MyApp({ Component, pageProps, router }) {
         <link rel="icon" href="/logo-mini.png" />
         <meta name="description" content="Kto z nas nie uwielbia odprężyć się w saunie czy jacuzzi? Specjalnie dla was wkomponowaliśmy te dwie atrakcje w przepiękne otoczenie natury!" />
         <meta name="keywords" content="Dziki staw, sauna, olkusz, wypoczynek, jacuzzi olkusz, sauna olkusz"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet"/> 
       </Head>
       <AnimatePresence mode="wait">
           <Component {...pageProps} key={router.route} />  
